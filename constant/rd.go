289ffa36073fe611623ba9815a4d24bafb55d936
uchenna <uchenna.anyanwu@tron.network>
2019-11-15 18:50:24 -0800

    [BTFS-983] Added parser, redisconn functions, and constants in rd

---
+++ constant/rd.go
@@ -0,0 +1,11 @@
+package constant
+
+const (
+	// -- ERRORS --
+	// connection
+	RDConnectionError       = "Cannot connect to the RD database!"
+	RDURLParseError			= "Cannot parse redis connection URL!"
+
+	// -- DEBUG --
+	RDConnectionHealthy = "Connection to redis is healthy."
+)
