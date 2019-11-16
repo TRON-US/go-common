ed286c0568987ebf4422273c5d03becb2a853135
uchenna <uchenna.anyanwu@tron.network>
2019-11-15 18:54:41 -0800

    [BTFS-983] - ran fmt

--- constant/rd.go
+++ constant/rd.go
@@ -3,8 +3,8 @@ package constant
 const (
 	// -- ERRORS --
 	// connection
-	RDConnectionError       = "Cannot connect to the RD database!"
-	RDURLParseError			= "Cannot parse redis connection URL!"
+	RDConnectionError = "Cannot connect to the RD database!"
+	RDURLParseError   = "Cannot parse redis connection URL!"
 
 	// -- DEBUG --
 	RDConnectionHealthy = "Connection to redis is healthy."
