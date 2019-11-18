bf232fc4716ff3289e2692b370f1b6f2648070dd
uchenna <uchenna.anyanwu@tron.network>
2019-11-17 16:30:50 -0800

    [BTFS-974] - functions added

--- handlers/grpcpanichandler.go
+++ handlers/grpcpanichandler.go
@@ -1,12 +1,29 @@
 package handlers
 
 import (
-panichandler "github.com/kazegusuri/grpc-panic-handler"
-"google.golang.org/grpc"
+	"errors"
+	"runtime"
+
+	"github.com/tron-us/go-common/log"
+	"go.uber.org/zap"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+
+	grpc_recovery "github.com/grpc-ecosystem/go-grpc-middleware/recovery"
 )
 
 var (
-	UIntOpt = grpc.UnaryInterceptor(panichandler.UnaryPanicHandler)
-	SIntOpt = grpc.StreamInterceptor(panichandler.StreamPanicHandler)
-)
+	// Panic handler prints the stack trace when recovering from a panic.
+	RecoveryCustomFunc grpc_recovery.RecoveryHandlerFunc = grpc_recovery.RecoveryHandlerFunc(func(p interface{}) error {
+		buf := make([]byte, 1<<16)
+		stacklen := runtime.Stack(buf, true)
+		log.Error("Panic attack :", zap.Error(errors.New(string(buf[:stacklen]))))
+		return status.Errorf(codes.Internal, "%s", p)
+	})
+	// Shared options for the logger, with a custom gRPC code to log level function.
+	Opts = []grpc_recovery.Option{
+		grpc_recovery.WithRecoveryHandler(RecoveryCustomFunc),
+	}
 
+	UnaryServerInterceptor = grpc_recovery.UnaryServerInterceptor(Opts...)
+)
