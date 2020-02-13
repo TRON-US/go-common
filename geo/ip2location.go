dc1bc3bfb3649aec4adf526065b85e8305205f60
Weiyu Xue <wxue2012@gmail.com>
2020-02-12 16:38:42 -0800

    add geo package

---
+++ geo/ip2location.go
@@ -0,0 +1,45 @@
+package geo
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/ip2location/ip2location-go"
+	"google.golang.org/grpc/metadata"
+)
+
+// package init will load bin file in memory
+func init() {
+	ip2location.Open("./IP2LOCATION-LITE-DB5.BIN")
+}
+
+// Ip2Location gets ipv4 address,
+// and returns country_short, region, latitude and longitude
+func Ip2Location(ip string) (string, string, float32, float32) {
+	results := ip2location.Get_all(ip)
+	return results.Country_short, results.Region, results.Latitude, results.Longitude
+}
+
+// ExtractIp extract ipv4 adress from context
+// that passed down from nginx ingress server
+func ExtractIp(ctx context.Context) (string, error) {
+	addr := ""
+	md, ok := metadata.FromIncomingContext(ctx)
+	if !ok {
+		return "", fmt.Errorf("failed to get metadata")
+	}
+	if len(md["x-real-ip"]) != 0 {
+		addr = md["x-real-ip"][0]
+	} else if len(md[":authority"]) != 0 {
+		s := strings.Split(md[":authority"][0], ":")
+		if len(s) == 0 {
+			return "", fmt.Errorf("failed to get local ip")
+		}
+		addr = s[0]
+	} else {
+		return "", fmt.Errorf("failed to get real ip")
+	}
+	return addr, nil
+}
--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/go-pg/pg/v9 v9.0.1
 	github.com/go-redis/redis/v7 v7.0.0-beta.4
 	github.com/grpc-ecosystem/go-grpc-middleware v1.1.0
+	github.com/ip2location/ip2location-go v8.2.0+incompatible
 	github.com/json-iterator/go v1.1.6
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.1 // indirect
