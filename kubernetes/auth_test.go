7823cda0afad09de6ec8f2a78f4cf7920df0a330
Ashish Kulkarni <thatInfrastructureGuy@gmail.com>
2020-03-20 15:39:36 -0700

    Add tests around kube authentication
    Signed-off-by: Ashish Kulkarni <thatInfrastructureGuy@gmail.com>

--- kubernetes/auth_test.go
+++ kubernetes/auth_test.go
@@ -1,6 +1,9 @@
 package kubernetes
 
 import (
+	"os"
+	"testing"
+
 	"k8s.io/client-go/kubernetes/fake"
 )
 
@@ -8,4 +11,34 @@ func newFakeClient() *Config {
 	k := Config{}
 	k.clientset = fake.NewSimpleClientset()
 	return &k
+}
+
+func TestAuthenticationDefaultError(t *testing.T) {
+	k := newFakeClient()
+	err := k.Authenticate()
+	if err == nil {
+		t.Error(err)
+	}
+}
+
+func TestAuthenticationKubeFileError(t *testing.T) {
+	k := newFakeClient()
+	kubeconfig := "/thefilewhichdoesnotExists"
+	os.Setenv("KUBECONFIG", kubeconfig)
+	err := k.Authenticate()
+	if !fileExists(kubeconfig) {
+		if err == nil {
+			t.Error("Kubeconfig env var set but file not present should error out")
+		}
+	}
+}
+
+func TestAuthenticationKubeConfigError(t *testing.T) {
+	k := newFakeClient()
+	kubeconfig := "./testdata/corruptedkubeconfig"
+	os.Setenv("KUBECONFIG", kubeconfig)
+	err := k.Authenticate()
+	if err == nil {
+		t.Error("Corrupted Kubeconfig should throw an error")
+	}
 }
